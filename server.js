032c5bac90502924861659a8b66d3a9e4f883485
J. Munford <jaemunford@gmail.com>
2016-03-01 15:25:21 -0500

    fixed index.html routing

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var datepicker = require('./datepicker.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-	res.send(index.html);
+	res.render('index');
 });
 
 app.get('/:time', function(req, res) {
